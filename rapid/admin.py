f9fcc6d75bb29935d0eed8f7fff58c73609e82ca
Corey Oordt <coordt@washingtontimes.com>
2011-04-28 20:58:00 -0400

    Added Containers to the admin for manual container creation.

--- rapid/admin.py
+++ rapid/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from models import Account
+from models import Account, Container
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('user', )
 
+class ContainerAdmin(admin.ModelAdmin):
+    fields = ('name', 'path', 'account')
+    list_display = ('name', 'path', 'account')
+    list_filter = ('account',)
+    search_fields = ('name', )
+
+admin.site.register(Container, ContainerAdmin)
 admin.site.register(Account, AccountAdmin)
+
