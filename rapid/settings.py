06aee7f88a43321a49fc5a7a0f883a588c0467d4
Corey Oordt <coordt@washingtontimes.com>
2011-04-28 20:58:13 -0400

    Removed outdated settings.

--- rapid/settings.py
+++ rapid/settings.py
@@ -1,7 +1,3 @@
 from django.conf import settings
 
-STORAGE_URL = '/storage/v1/'
-MANAGEMENT_URL = '/management/v1/'
-DEFAULT_ACCOUNT = 'default'
-
 CONTAINER_LOCATION = getattr(settings, 'CONTAINER_LOCATION', 'storage')
